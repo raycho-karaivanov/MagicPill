18e2460a6821772bb4ebcf6e7de386f72a728161
Raycho Karaivanov <raycho.karaivanov@gmail.com>
2016-11-14 12:00:54 +0200

    Initial Commit

---
+++ MagicPill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicPill
+//
+//  Created by Raycho Karaivanov on 14/11/2016.
+//  Copyright © 2016 Raycho Karaivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
