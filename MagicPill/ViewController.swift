607331c3c3f99db1c4f5e19a53cf48035a49d820
Raycho Karaivanov <raycho.karaivanov@gmail.com>
2016-11-17 12:36:41 +0200

    Added country text field
    Added country text field and Buy button

--- MagicPill/ViewController.swift
+++ MagicPill/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  MagicPill
@@ -8,18 +9,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var buyNowBtn: UIButton!
+    @IBOutlet weak var pillPic: UIImageView!
+    @IBOutlet weak var magicLbl: UILabel!
+    @IBOutlet weak var lineImg: UIView!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var addressLbl: UILabel!
+    @IBOutlet weak var addressTxt: UITextField!
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var cityTxt: UITextField!
+    @IBOutlet weak var countryLbl: UILabel!
+    @IBOutlet weak var zipLbl: UILabel!
+    @IBOutlet weak var zipTxt: UITextField!
+    @IBOutlet weak var successImg: UIImageView!
+    @IBOutlet weak var countryTxt: UITextField!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    
+    let states = ["Austria", "Belgium", "Bulgaria", "Croatia", "Republic of Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden", "UK"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        
+        statePicker.isHidden = false
+        zipLbl.isHidden = true
+        zipTxt.isHidden = true
+        buyNowBtn.isHidden = true
     }
+    
+    @IBAction func buyNowBtnPressed(_ sender: Any) {
+        
+        
+    }
+   
+    @IBAction func countryTxtPressed(_ sender: Any) {
+        statePicker.isHidden = false
+       // countryTxt.isUserInteractionEnabled = false
+        zipLbl.isHidden = true
+        zipTxt.isHidden = true
+        buyNowBtn.isHidden = true
+    }
+    
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        countryTxt.text = states[row]
+       // countryTxt.isUserInteractionEnabled = false
+        statePicker.isHidden = true
+        zipLbl.isHidden = false
+        zipTxt.isHidden = false
+        buyNowBtn.isHidden = false
+        
+    }
 }
 
